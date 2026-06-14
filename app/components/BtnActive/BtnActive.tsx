073fef41f87187a9359c9616f57b3e3fc4fee941
Ruan Cancio <slidruan@gmail.com>
2026-06-13 21:59:47 -0300

    update button

--- app/components/BtnActive/BtnActive.tsx
+++ app/components/BtnActive/BtnActive.tsx
@@ -1,26 +1,58 @@
 "use client"
-import { useState, useEffect} from "react"
+
 import { useList } from "@/context/ListContext"
 
-export default function BtnActive({ name }: { name: string }) {
+export default function BtnActive({
+    name
+}: {
+    name: string
+}) {
 
     const { list, setList } = useList()
-    const filtered = list.filter(current => current.name === name)[0]
-    filtered.isActive = !filtered.isActive
+
+    const filtered = list.find(
+        current => current.name === name
+    )
 
     const BtnToggle = (title: string) => {
-        let filterState = list.filter(current => current.name === title)[0].isActive
-        let thisElement = list.filter(current => current.name === title)[0]
-        let listCopy = [...list]
-        thisElement.isActive = !filterState
-        setList(listCopy)
+
+        const newList = list.map(item => {
+
+            if (item.name === title) {
+                return {
+                    ...item,
+                    isActive: !item.isActive
+                }
+            }
+
+            return item
+        })
+
+        setList(newList)
     }
 
     return (
-        <div onClick={() => {
-            BtnToggle(name)
-        }} className={`bg-gray-300 dark:bg-gray-500 cursor-pointer rounded-4xl w-12.5 h-7 px-0.5 py-2 flex items-center  ${filtered.isActive ? "justify-end bg-red-400 dark:bg-red-400" : "justify-start"}`}>
-            <button className="bg-white cursor-pointer rounded-full w-6 h-6"></button>
+        <div
+            onClick={() => BtnToggle(name)}
+            className={`
+                bg-gray-300
+                dark:bg-gray-500
+                cursor-pointer
+                rounded-4xl
+                w-12.5
+                h-7
+                px-0.5
+                py-2
+                flex
+                items-center
+                ${filtered?.isActive
+                    ? "justify-end bg-red-400 dark:bg-red-400"
+                    : "justify-start"}
+            `}
+        >
+            <button
+                className="bg-white cursor-pointer rounded-full w-6 h-6"
+            />
         </div>
     )
 }
